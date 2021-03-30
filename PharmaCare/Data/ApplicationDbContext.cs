de819c15aa3573848b83b59543f2b8c21aa22282
zakariabijoy <zakaria@vonome.com>
2021-03-30 21:33:38 +0600

    Add project files.

---
+++ PharmaCare/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PharmaCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaCare.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ PharmaCare/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaCare.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
